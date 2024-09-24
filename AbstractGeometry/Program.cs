f9d46a861a1bd2582a9faf5a1c62b1e23723612b
Alexander Melikhov <alexandersermel@gmail.com>
2024-09-25 00:38:25 +0300

    Interface done

---
+++ AbstractGeometry/Program.cs
@@ -0,0 +1,64 @@
+﻿//#define CHECK_1
+//#define CHECK_2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AbstractGeometry
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+#if true
+#if CHECK_1
+
+            IntPtr hwnd = GetConsoleWindow();
+            Graphics graphics = Graphics.FromHwnd(hwnd);
+            System.Drawing.Rectangle window_rect = new System.Drawing.Rectangle
+            (
+            Console.WindowLeft, Console.WindowTop,
+            Console.WindowWidth, Console.WindowHeight
+            );
+            PaintEventArgs e = new PaintEventArgs(graphics, window_rect);
+            Rectangle rectangle = new Rectangle(80, 50, 300, 50, 2, Color.AliceBlue);
+            rectangle.Info(e);
+
+            Square square = new Square(75, 500, 50, 3, Color.Red);
+            square.Info(e);
+            Circle circle = new Circle(80, 800, 50, 1, Color.Yellow);
+            circle.Info(e);  
+#endif
+#endif
+        }
+#if CHECK_2
+        Shape[] shapes = new Shape[] 
+        {
+            new Rectangle(80,50,400,60,2, Color.AliceBlue),
+            new Square(75,500,50,3, Color.AliceBlue),
+            new Circle(800,700,50,1, Color.AliceBlue),
+        };
+        for(int i=0;i<shape.Length,i++)
+        {
+           //if(shapes[i] is IHaveDiagonal)
+           //shapes[i].Info(e);
+           //if(shapes[i] is IHaveDiameter)
+           //shapes[i].Info(e);
+        }
+
+
+#endif
+
+        [DllImport("kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+    }
+    
+}
---
+++ AbstractGeometry/Shape.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace AbstractGeometry
+{
+    abstract class Shape
+    {
+       
+        
+        public static readonly int MIN_START_X = 150;
+        public static readonly int MAX_START_X = 80;
+        public static readonly int MIN_START_Y = 100;
+        public static readonly int MAX_START_Y = 150;
+        public static readonly int MAX_LINE_WIDTH = 16;
+        public static readonly int MIN_LINE_WIDTH = 1;
+        public static readonly Color DEFAULT_COLOR = Color.AliceBlue;
+        public static readonly int MIN_SIZE = 20;
+        public static readonly int MAX_SIZE = 120;
+
+            int start_x;
+            int start_y;
+            int line_width;
+            public Color Color { get; set; }
+
+        public int Start_X
+        { 
+            get=> start_x;
+            set
+            {
+                if (value < MIN_START_X) value = MIN_START_X;
+                if (value < MAX_START_X) value = MAX_START_X;
+                this.start_x = value;
+
+            }
+        
+        }
+        public int Start_Y
+        {
+            get => start_y;
+            set=>start_y=
+                value<MIN_START_Y?MIN_START_Y:
+                value >MAX_START_Y?MAX_START_Y:
+                value;
+        }
+        public int LineWidth
+        {
+            get => line_width;
+            set=>line_width=
+                value < MIN_LINE_WIDTH ? MIN_LINE_WIDTH:
+                value >MAX_LINE_WIDTH ? MAX_LINE_WIDTH:
+                value;
+        }
+
+                    //Constructors:
+
+        public Shape(int start_x,int start_y,int line_width,Color color)
+            {
+                Start_X=start_x;
+                Start_Y=start_y;
+                LineWidth=line_width;
+                Color=color;
+            }
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+        public abstract void Draw(PaintEventArgs e);
+        public virtual void Info(PaintEventArgs e)
+        {
+            Console.WriteLine($"Площадь фигуры:{GetArea()}");
+            Console.WriteLine($"Периметр фигуры:{GetPerimeter()}");
+            Draw(e);
+        }
+                       
+    }
+}
